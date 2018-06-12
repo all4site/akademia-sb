a5ab456c1f16ab52fe562ec50e29f988e45e38e7
All4site <all4sitecomua@gmail.com>
2018-06-12 19:02:38 +0300

    Initial commit

---
+++ app/js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+	$('.collapsible').collapsible();
+});
+
+$(document).ready(function () {
+	var height = $(this).height() - 10;
+
+	$(window).on('scroll', function () {
+		var topOffset = $(this).scrollTop();
+		var topTop = $(this).height();
+		var element = $('#content').offset().top;
+		console.log(element);
+		console.log(topOffset);
+		if (topOffset >= element) {
+			$('.robik').addClass('robik-fixed');
+		}
+		if (topOffset <= element) {
+			$('.robik').removeClass('robik-fixed');
+		}
+
+
+	});
+
+});
